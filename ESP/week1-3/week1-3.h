6a263b7200d7d41f2cb1c660e7a6df1faaef7af1
KingSlayer260 <collin.smit@hotmail.com>
2023-12-06 13:17:10 +0100

    week 1 esp

---
+++ ESP/week1-3/week1-3.h
@@ -0,0 +1,8 @@
+#ifndef WEEK1_3_H  // Header guards to prevent multiple inclusions
+#define WEEK1_3_H
+
+// data below is set as a define in the file secret.h
+const char* ssid = "Godspot (wifi van de kerk)"; // mijn thuis wifi omdat we deze opdracht niet meer op school konden maken
+const char* password = "geheim :)";
+
+#endif
